6888949ab4083f1df0997860f8a16347dc985b69
bacti <tranmhaii@gmail.com>
2018-05-03 09:44:54 +0700

    +

--- js/main.js
+++ js/main.js
@@ -34,6 +34,6 @@ class App extends React.Component
 
 window.onload = function()
 {
-    $('<div></div>').attr('id', 'root').appendTo(document.body)
-    ReactDOM.render(<App/>, document.getElementById('root'))
+    $('<div></div>').attr('id', 'sceneContainer').css({ width: '100%', height: '100%' }).appendTo(document.body)
+    ReactDOM.render(<App/>, document.getElementById('sceneContainer'))
 }
