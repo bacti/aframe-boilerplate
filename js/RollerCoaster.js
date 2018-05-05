3f02b7019cf7de534c9f1ab967d8e894c9f8740a
Hai Bacti <haibacti@MacCandy.local>
2018-05-05 12:08:55 +0700

    +

--- js/RollerCoaster.js
+++ js/RollerCoaster.js
@@ -7,10 +7,7 @@ export default class RollerCoaster extends React.Component
     {
         super(props, context)
 
-        // construct the position vector here, because if we use 'new' within render,
-        // React will think that things have changed when they have not.
         this.cameraPosition = new THREE.Vector3(0, 0, 5)
-  
         this.state =
         {
             cubeRotation: new THREE.Euler(),
@@ -20,42 +17,26 @@ export default class RollerCoaster extends React.Component
         {
             this.setState(
             {
-                cubeRotation: new THREE.Euler(
-                    this.state.cubeRotation.x + 0.1,
-                    this.state.cubeRotation.y + 0.1,
-                    0
-                ),
+                cubeRotation: new THREE.Euler(this.state.cubeRotation.x + 0.1, this.state.cubeRotation.y + 0.1, 0),
             })
         }
     }
   
     render()
     {
-        const width = window.innerWidth
-        const height = window.innerHeight
-  
+        let width = window.innerWidth
+        let height = window.innerHeight
         return (
-            <React3
-                mainCamera='perspective'
-                width={width}
-                height={height}
+            <React3 mainCamera='perspective' width={width} height={height}
                 onAnimate={this.OnAnimate}
             >
                 <scene>
-                    <perspectiveCamera
-                        name='perspective'
-                        fov={75}
-                        aspect={width / height}
-                        near={0.1}
-                        far={1000}
+                    <perspectiveCamera name='perspective'
+                        fov={75} aspect={width / height} near={0.1} far={1000}
                         position={this.cameraPosition}
                     />
                     <mesh rotation={this.state.cubeRotation}>
-                        <boxGeometry
-                            width={1}
-                            height={1}
-                            depth={1}
-                        />
+                        <boxGeometry width={1} height={1} depth={1} />
                         <meshBasicMaterial color={0x00ff00} />
                     </mesh>
                 </scene>
