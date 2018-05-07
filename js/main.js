1695ef9621e15d47109430c24b3a944fe14b7309
bacti <tranmhaii@gmail.com>
2018-05-07 14:02:59 +0700

    + drop aframe

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 require('./zepto')
-// import '../libs/aframe/src'
-// import { Entity, Scene } from 'aframe-react'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+
 import RollerCoaster from './RollerCoaster'
+import reducers from './reducers'
 
 class App extends React.Component
 {
@@ -14,22 +16,11 @@ class App extends React.Component
 
     render()
     {
-        return <RollerCoaster canvas={document.getElementById('mainCanvas')} />
-        // return (
-        //     <Scene>
-        //         <Entity primitive='a-box' position='-1 0.5 -3' rotation='0 45 0' color='#4CC3D9' />
-        //         <Entity primitive='a-sphere' position='0 1.25 -5' radius='1.25' color='#EF2D5E' />
-        //         <Entity primitive='a-cylinder' position='1 0.75 -3' radius='0.5' height='1.5' color='#FFC65D' />
-        //         <Entity primitive='a-plane' position='0 0 -4' rotation='-90 0 0' width='4' height='4' color='#7BC8A4' />
-        //         <Entity primitive='a-sky' color='#ECECEC' />
-
-        //         <Entity primitive='a-camera'>
-        //             <Entity primitive='a-cursor'
-        //                 animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}
-        //             />
-        //         </Entity>
-        //     </Scene>
-        // )
+        return (
+            <Provider store={applyMiddleware()(createStore)(reducers)}>
+                <RollerCoaster canvas={document.getElementById('mainCanvas')} />
+            </Provider>
+        )
     }
 }
 
