5e5cd9999c7b3401372a399c5c3fd06a1fc99bfc
bacti <tranmhaii@gmail.com>
2018-05-07 11:53:22 +0700

    +

--- js/Landscape.js
+++ js/Landscape.js
@@ -5,23 +5,40 @@ export default class Landscape extends React.Component
     constructor(props)
     {
 		super(props)
-		this.InitGeometry()
+    }
+
+    componentWillMount()
+    {
+        let geometry = this.refs.geometry
+        geometry.rotateX( - Math.PI / 2 );
+        
+        let positions = geometry.attributes.position.array;
+        let vertex = new THREE.Vector3();
+        
+        for ( var i = 0; i < positions.length; i += 3 ) {
+        
+            vertex.fromArray( positions, i );
+        
+            vertex.x += Math.random() * 10 - 5;
+            vertex.z += Math.random() * 10 - 5;
+        
+            var distance = ( vertex.distanceTo( scene.position ) / 5 ) - 25;
+            vertex.y = Math.random() * Math.max( 0, distance );
+        
+            vertex.toArray( positions, i );
+        
+        }
+        
+        geometry.computeVertexNormals();
     }
 
     render()
     {
 		return (
             <mesh>
-                <bufferGeometry
-                    position={new THREE.BufferAttribute( new Float32Array( this.vertices ), 3 )}
-                />
-                <meshBasicMaterial color={0xffffff} />
+                <planeBufferGeometry ref='geometry' width={500} height={500} widthSegments={15} heightSegments={15} />
+                <meshLambertMaterial color={0x407000} />
             </mesh>
 		)
-    }
-    
-    InitGeometry()
-    {
-        this.vertices = []
     }
 }
