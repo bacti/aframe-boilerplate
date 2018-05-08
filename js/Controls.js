5509359fecf2ab5c4664ac46a88edc164a06a74a
bacti <tranmhaii@gmail.com>
2018-05-08 15:47:34 +0700

    +

---
+++ js/Controls.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import React3 from '../libs/react-three-renderer/src' 
+
+class RollerCoaster extends React.Component
+{
+    constructor(props, context)
+    {
+        super(props, context)
+
+        this.OnAnimate = _ =>
+        {
+        }
+    }
+
+    componentDidMount()
+    {
+    }
+
+    render()
+    {
+        let width = window.innerWidth
+        let height = window.innerHeight
+        return (
+            <React3 mainCamera='perspective' width={width} height={height} antialias={true}
+                canvas={this.props.canvas}
+                onAnimate={this.OnAnimate}
+            >
+            </React3>
+        )
+    }
+}
