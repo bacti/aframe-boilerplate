d9c1e3d4ef5a5ae6ded1a4346bcd538bdc6f52d5
bacti <tranmhaii@gmail.com>
2018-05-08 10:42:26 +0700

    +

--- js/WebVR.js
+++ js/WebVR.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 window.hasNativeWebVRImplementation = !!window.navigator.getVRDisplays || !!window.navigator.getVRDevices
-import WebVRPolyfill from 'webvr-polyfill'
+import WebVRPolyfill from '../libs/webvr-polyfill/src'
 window.webvrpolyfill = new WebVRPolyfill(
 {
   	BUFFER_SCALE: 0.5,
