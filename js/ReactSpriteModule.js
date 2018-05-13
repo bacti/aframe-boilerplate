393b4c53444e2e60708492c03c890e4ff463668a
Hai Bacti <haibacti@MacCandy.local>
2018-05-13 11:03:52 +0700

    +

---
+++ js/ReactSpriteModule.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class ReactSpriteModule extends React.Component
+{
+    constructor(props)
+    {
+        super(props)
+    }
+
+	render()
+	{
+		return <object3D></object3D>
+	}
+}
