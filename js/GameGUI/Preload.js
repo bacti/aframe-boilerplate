5a2983792d25ef949ac4025916a770651299f544
bacti <tranmhaii@gmail.com>
2018-05-14 16:36:55 +0700

    +

--- js/GameGUI/Preload.js
+++ js/GameGUI/Preload.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import Loader from 'resource-loader'
 import Jasmine from '../../libs/jasmine/'
 import Actions from '../actions/'
 
@@ -9,13 +10,25 @@ class Preload extends React.Component
     constructor(props)
     {
         super(props)
-
         resource.load_buffer(resource.get_embed_src('data/all1.bsprite'), response =>
         {
             new Jasmine.Loader(response, pixma =>
             {
-                console.log(pixma)
-                this.props.SwitchState('SPLASH')
+                let images = pixma.images
+                images.push(...
+                [
+                    'image/vr-icon.png',
+                    'image/crate.png',
+                ])
+                
+                let textureLoader = new Loader()
+                images.forEach(imageUrl => {
+                    textureLoader.add(imageUrl, resource.get_embed_src(`data/${imageUrl}`), { loadType: Loader.Resource.LOAD_TYPE.XHR });
+                })
+                textureLoader.load((loader, resources) =>
+                {
+                    this.props.SwitchState('SPLASH')
+                })
             })
         })
     }
