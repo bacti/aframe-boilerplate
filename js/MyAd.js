fd3ac2cf9e8b292a63afac6046b23f0aa5781ed6
bacti <tranmhaii@gmail.com>
2018-05-14 13:44:06 +0700

    +

--- js/MyAd.js
+++ js/MyAd.js
@@ -1,25 +1,39 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-
-import Preload from './screens/Preload'
-import RollerCoaster from './RollerCoaster/'
+import React3 from '../libs/react-three-renderer/src' 
 
 export default class MyAd extends React.Component
 {
     constructor(props)
     {
         super(props)
+        this.OnAnimate = _ =>
+        {
+        }
     }
 
 	render()
 	{
+        let width = window.innerWidth
+        let height = window.innerHeight
         return (
-            <BrowserRouter>
-                <div>
-                    <Route exact path='/' component={Preload}/>
-                    <Route exact path='/roller-coaster' component={RollerCoaster}/>
-                </div>
-            </BrowserRouter>
+            <React3 mainCamera='camera' orthoCamera='orthocamera' width={width} height={height} antialias={true}
+                canvas={document.getElementById('mainCanvas')}
+                onAnimate={this.OnAnimate}
+            >
+                <scene background={0xf0f0ff} ref='scene'>
+                    <perspectiveCamera name='camera' ref='camera'
+                        fov={40} aspect={width/height} near={0.1} far={500}
+                    />
+                </scene>
+                <orthoscene ref='orthoscene'>
+                    <orthographicCamera name='orthocamera' ref='orthocamera'
+                        position={new THREE.Vector3(0, 0, 1)}
+                        left={width / - 2} right={width / 2}
+                        top={height / 2} bottom={height / - 2}
+                        near={1} far={100}
+                    />
+                </orthoscene>
+            </React3>
         )
 	}
 }
