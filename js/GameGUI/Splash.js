7126a8a41d1ca6a9aaa671f5d23eea01d52805ca
bacti <tranmhaii@gmail.com>
2018-05-15 15:03:25 +0700

    +

--- js/GameGUI/Splash.js
+++ js/GameGUI/Splash.js
@@ -7,20 +7,19 @@ export default class Splash extends React.Component
         super(props)
     }
 
-	componentDidMount()
+	componentWillMount()
 	{
+        this.texCrate = resource.textures['image/crate.png']
     }
 
 	render()
 	{
         return (
             <sprite ref='sprite'
-                scale={new THREE.Vector3(256, 256, 1)}
+                scale={new THREE.Vector3(this.texCrate.image.width, this.texCrate.image.height, 1)}
                 position={new THREE.Vector3(window.innerWidth / 2, window.innerHeight / 2, 0)}
             >
-                <spriteMaterial>
-                    <texture url={(window.URL ? URL : webkitURL).createObjectURL(resource.data['image/crate.png'].data)} />
-                </spriteMaterial>
+                <spriteMaterial map={resource.textures['image/crate.png']} />
             </sprite>
         )
 	}
