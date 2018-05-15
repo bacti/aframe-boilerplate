7199d09bb76cb67ed419b4fc978890ab42b47be7
Hai Bacti <haibacti@MacCandy.local>
2018-05-15 23:13:26 +0700

    +

--- js/GameGUI/Preload.js
+++ js/GameGUI/Preload.js
@@ -1,3 +1,4 @@
+import JSZip from 'jszip'
 import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
@@ -10,34 +11,46 @@ class Preload extends React.Component
     constructor(props)
     {
         super(props)
-        resource.load_buffer(resource.get_embed_src('data/all1.bsprite'), response =>
+        resource.load_buffer(resource.get_embed_src('data/all1.zip'), response =>
         {
-            new Jasmine.Loader(response, sprite =>
+            let zip = new JSZip()
+            zip.loadAsync(response).then(zip =>
             {
-                this.props.AuroraLoader(sprite)
-                let images = sprite.images
-                images.push(...
-                [
-                    'image/crate.png',
-                    'image/room.jpg',
-                    'image/bg-interstitial.jpg',
-                ])
-                
-                let textureLoader = new THREE.TextureLoader()
-                let loader = images.map(imageUrl =>
+                for (let filename in zip.files)
                 {
-                    return new Promise((resolve, reject) =>
+                    if (filename.includes('.sprite'))
                     {
-                        textureLoader.load(resource.get_embed_src(`data/${imageUrl}`),
-                                texture => resolve({ alias: imageUrl, data: texture }), undefined, reject)
-                    })
-                })
-                Promise.all(loader).then(textures =>
-                {
-                    resource.textures = {}
-                    textures.map(texture => resource.textures[texture.alias] = texture.data)
-                    this.props.SwitchState('SPLASH')
-                })
+                        zip.file(filename).async('string').then(data =>
+                        {
+                            let sprite = JSON.parse(data)
+                            this.props.AuroraLoader(sprite)
+
+                            let images = sprite.images
+                            images.push(...
+                            [
+                                'image/crate.png',
+                                'image/room.jpg',
+                                'image/bg-interstitial.jpg',
+                            ])
+                            
+                            let textureLoader = new THREE.TextureLoader()
+                            let loader = images.map(imageUrl =>
+                            {
+                                return new Promise((resolve, reject) =>
+                                {
+                                    textureLoader.load(resource.get_embed_src(`data/${imageUrl}`),
+                                            texture => resolve({ alias: imageUrl, data: texture }), undefined, reject)
+                                })
+                            })
+                            Promise.all(loader).then(textures =>
+                            {
+                                resource.textures = {}
+                                textures.map(texture => resource.textures[texture.alias] = texture.data)
+                                this.props.SwitchState('SPLASH')
+                            })
+                        })
+                    }
+                }
             })
         })
     }
