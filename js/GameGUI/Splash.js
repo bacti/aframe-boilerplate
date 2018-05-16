d2228154990bf70ef520ef731711785b1093d0c3
bacti <tranmhaii@gmail.com>
2018-05-16 11:42:46 +0700

    +

--- js/GameGUI/Splash.js
+++ js/GameGUI/Splash.js
@@ -19,7 +19,7 @@ class Splash extends React.Component
 	{
         return (
             <object3D>
-                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_LOADING} />
+                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_AIM} />
                 <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_SLINGSHOT} />
                 <sprite ref='sprite'
                     scale={new THREE.Vector3(this.interstitial.width, this.interstitial.height, 1)}
