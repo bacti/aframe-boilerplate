33aa5a13cec1a71b7e8ba69d7972de4f107f5993
bacti <tranmhaii@gmail.com>
2018-05-17 16:00:12 +0700

    +

---
+++ js/GameWorld/VideoBackground.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class Background extends React.Component
+{
+    componentDidMount()
+    {
+        this.refs.sphere.scale(-1, 1, 1)
+    }
+
+    render()
+    {
+        return (
+            <mesh>
+                <sphereGeometry ref='sphere' radius={500} widthSegments={60} heightSegments={40} />
+                <meshBasicMaterial map={resource.textures['room.jpg']} />
+            </mesh>
+        )
+    }
+}
