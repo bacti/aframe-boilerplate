80032284175436855a2ca4f82364b177652a86df
bacti <tranmhaii@gmail.com>
2018-05-17 16:48:30 +0700

    +

--- js/GameGUI/Splash.js
+++ js/GameGUI/Splash.js
@@ -39,15 +39,15 @@ class Splash extends React.Component
 	{
         return (
             <object3D>
-                <Jasmine.ThreeSpriteFrame ref='begin' metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_TAP_CONTINUE} />
-                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_AIM} />
-                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_GL_LOGO} />
-                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_NEW_LOGO} />
-                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_TXT_1} />
-                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_TXT_2} />
-                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_TXT_3} />
-                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_ADS_TXT} />
-                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_ALL1_FRAME_SLINGSHOT} />
+                <Jasmine.ThreeSpriteFrame ref='begin' metadata={this.props.aurora} id={DataHeader.SPRITE_PRELOAD_FRAME_TAP_CONTINUE} />
+                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_PRELOAD_FRAME_AIM} />
+                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_PRELOAD_FRAME_GL_LOGO} />
+                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_PRELOAD_FRAME_NEW_LOGO} />
+                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_PRELOAD_FRAME_TXT_1} />
+                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_PRELOAD_FRAME_TXT_2} />
+                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_PRELOAD_FRAME_TXT_3} />
+                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_PRELOAD_FRAME_ADS_TXT} />
+                <Jasmine.ThreeSpriteFrame metadata={this.props.aurora} id={DataHeader.SPRITE_PRELOAD_FRAME_SLINGSHOT} />
                 <sprite ref='bg'
                     scale={new THREE.Vector3(this.interstitial.image.width, this.interstitial.image.height, 1)}
                 >
